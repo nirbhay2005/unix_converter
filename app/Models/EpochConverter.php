fdd4a78f138476d32e1a275b6c07189a80b06c46
nirbhay2005 <nirbhay2005@gmail.com>
2020-10-22 17:59:27 +0530

    Model: Added getFirstAndLastOfInterval() function in EpochConverter model

--- app/Models/EpochConverter.php
+++ app/Models/EpochConverter.php
@@ -29,8 +29,9 @@ class EpochConverter
         ];
     }
 
-    public function getTimestampFromDate($date, $timeZone){
-        switch ($timeZone){
+    public function getTimestampFromDate($date, $timeZone)
+    {
+        switch ($timeZone) {
             case 'local' :
                 $date = Carbon::parse($date);
                 return Carbon::parse($date, $date->getTimezone())->getTimestamp();
@@ -38,5 +39,34 @@ class EpochConverter
             default:
                 return Carbon::parse($date, 'UTC')->getTimestamp();
         }
+    }
+
+    public function getFirstAndLastOfInterval($params)
+    {
+        $tz = $params['timezone'] == 'local' ? null : $params['timezone'];
+
+        switch ($params['format']) {
+            case 'year' :
+                $date = Carbon::create($year = $params['year'], $month = 1, $day = 1, $hour = 0, $minute = 0, $second = 0, $tz);
+                $start = Carbon::parse($date)->startOfYear();
+                $end = Carbon::parse($date)->endOfYear();
+                break;
+            case 'month' :
+                $date = Carbon::create($year = $params['year'], $month = $params['month'], $day = 1, $hour = 0, $minute = 0, $second = 0, $tz);
+                $start = Carbon::parse($date)->startOfMonth();
+                $end = Carbon::parse($date)->endOfMonth();
+                break;
+            case 'day' :
+                $date = Carbon::create($year = $params['year'], $month = $params['month'], $day = $params['day'], $hour = 0, $minute = 0, $second = 0, $tz);
+                $start = Carbon::parse($date)->startOfDay();
+                $end = Carbon::parse($date)->endOfDay();
+                break;
+        }
+        return [
+            'start_date' => $start->toCookieString(),
+            'start_timestamp' => $start->getTimestamp(),
+            'end_date' => $end->toCookieString(),
+            'end_timestamp' => $end->getTimestamp()
+        ];
     }
 }
