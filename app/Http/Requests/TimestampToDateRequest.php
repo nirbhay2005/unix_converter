592f5f28a3316352a26b6b13583bced6964b546c
nirbhay2005 <nirbhay2005@gmail.com>
2020-10-28 16:14:53 +0530

    Requests: Added validation rules for timestamp in TimestampToDateRequest

--- app/Http/Requests/TimestampToDateRequest.php
+++ app/Http/Requests/TimestampToDateRequest.php
@@ -1,17 +1,23 @@
 <?php
 
-
 namespace App\Http\Requests;
 
+use Illuminate\Foundation\Http\FormRequest;
 
-use Luezoid\Laravelcore\Requests\BaseRequest;
-
-class TimestampToDateRequest extends BaseRequest
+class TimestampToDateRequest extends FormRequest
 {
     public function rules()
     {
         return [
-            'timeStamp' => 'required|integer|digits_between:1,13',
+            'timestamp' => 'bail|required|integer|digits_between:1,13',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'timestamp.integer' => 'Invalid Timestamp',
+            'timestamp.digits_between' => 'Invalid Timestamp'
         ];
     }
 }
