4cdb6fce31320fa1f5669fd3f5e1feedcad468a1
nirbhay2005 <nirbhay2005@gmail.com>
2020-11-17 20:13:02 +0530

    Requests: Renamed FirstAndLastDateRequest => BeginEndDateRequest

--- app/Http/Requests/BeginEndDateRequest.php
+++ app/Http/Requests/BeginEndDateRequest.php
@@ -6,7 +6,7 @@ namespace App\Http\Requests;
 use Illuminate\Validation\Rule;
 use Luezoid\Laravelcore\Requests\BaseRequest;
 
-class FirstAndLastDateRequest extends BaseRequest
+class BeginEndDateRequest extends BaseRequest
 {
     public function rules()
     {
