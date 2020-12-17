0d9522dcfa5be4bfee356539d9dd6142991da1df
nirbhay2005 <nirbhay2005@gmail.com>
2020-12-17 16:19:21 +0530

    HeaderClock: Minor style changes.

--- resources/js/components/Homepage/HeaderClock.vue
+++ resources/js/components/Homepage/HeaderClock.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="mt-3 ml-4 mr-4 border rounded border-dark shadow">
-        <p class="mt-3 ml-2 mr-1">Current Unix Timestamp:
+    <div class="mt-3 ml-4 mr-4 mb-2 border rounded shadow-sm">
+        <p class="mt-3 ml-2 mr-1 mb-2">Current Unix Timestamp:
             <span class="font-weight-bold" style="text-shadow: 1px 1px grey"><label id="time"></label></span>
         </p>
     </div>
